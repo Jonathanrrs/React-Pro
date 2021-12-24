64d0b26c49e5859f8d0389adc1a8e03c008db3b1
jonathan <yoni_12rrs@hotmail.com>
2021-12-23 20:05:17 -0600

    Compound Component Pattern - Tercer Paso

--- 05-compound-component-pattern/src/02-component-patterns/components/ProductCard.tsx
+++ 05-compound-component-pattern/src/02-component-patterns/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'; /* es una interfaz de tipo generico */
+import { createContext, ReactElement, useContext } from 'react'; /* es una interfaz de tipo generico */
 import { useProduct } from '../hooks/useProduct';
 
 import styles from '../styles/styles.module.css';
@@ -15,27 +15,60 @@ interface Product {
     img?: string
 }
 
+interface ProductContextProps {
+    counter: number;
+    increaseBy: (value: number) => void;
+    product: Product
+
+}
+
+const ProductContext = createContext({} as ProductContextProps);
+
+const { Provider } = ProductContext;
+
 /* '' asi para que la img sea opcional, vacio para el ternario es que no tiene valor */
 export const ProductImage = ({ img = '' }) => {
+
+    const {product} = useContext(ProductContext);
+    let imgToShow: string;
+
+    if(img) {
+        imgToShow = img;
+    } else if(product.img) {
+        imgToShow = product.img
+    } else {
+        imgToShow = noImage;
+    }
+
+
     return (
-        <img className={styles.productImg} src={img ? img : noImage} alt="Product image" />
+        <img className={styles.productImg} src={imgToShow} alt="Product image" />
     );
 }
 
 /* se le obliga que venga el titulo, es una interfaz */
 export const ProductTitle = ({ title }: { title: string }) => {
+
+    const {product} = useContext(ProductContext)
+
     return (
-        <span className={styles.productDesciption}>{title}</span>
+        <span className={styles.productDesciption}>{title ? title : product.title}</span>
     );
 }
 
-interface ProductButtonsProps {
-    counter: number;
-    /* funcion que no regresa nada */
-    increaseBy: (value: number) => void;
-}
 
-export const ProductButtons = ({counter, increaseBy}: ProductButtonsProps) => {
+/* ya no se ocupa porque ahora lo tenemos del context */
+
+// interface ProductButtonsProps {
+//     counter: number;
+//     /* funcion que no regresa nada */
+//     increaseBy: (value: number) => void;
+// }
+
+export const ProductButtons = () => {
+
+    const {counter, increaseBy} = useContext(ProductContext)
+
     return (
         <div className={styles.buttonsContainer}>
             <button
@@ -59,15 +92,21 @@ export const ProductCard = ({ children, product }: Props) => {
     const { counter, increaseBy } = useProduct();
 
     return (
-        <div className={styles.productCard}>
-            {children}
+        <Provider value={{
+            counter,
+            increaseBy,
+            product
+        }}>
+            <div className={styles.productCard}>
+                {children}
 
-            {/* <ProductImage img={product.img} />
+                {/* <ProductImage img={product.img} />
             <ProductTitle title={product.title} />
 
             <ProductButtons counter={counter} increaseBy={increaseBy} /> */}
 
-        </div>
+            </div>
+        </Provider>
     )
 }
 
