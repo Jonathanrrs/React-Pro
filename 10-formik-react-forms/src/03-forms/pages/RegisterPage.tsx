e191e499f64bb37997d25c04f97c9b1c8c9947d2
jonathan <yoni_12rrs@hotmail.com>
2022-01-04 15:25:12 -0600

    Formulario tradicional - con useState

--- 10-formik-react-forms/src/03-forms/pages/RegisterPage.tsx
+++ 10-formik-react-forms/src/03-forms/pages/RegisterPage.tsx
@@ -1,6 +1,22 @@
+import { useState, ChangeEvent } from 'react';
 import '../styles/styles.css';
 
+
 export const RegisterPage = () => {
+
+    const [registerData, setRegisterData] = useState({
+        name: 'jonathan',
+        email: 'jonathan@gmail.com',
+        password1: '123456',
+        password2: '123456',
+    });
+
+    const {name, email, password1, password2} = registerData;
+
+    const onChange = (event:ChangeEvent<HTMLInputElement>)=> {
+        
+    }
+
     return (
         <div>
             <h1>Register Page</h1>
@@ -9,18 +25,26 @@ export const RegisterPage = () => {
             <input
                 type="text"
                 placeholder="Name"
+                value={name}
+                onChange={onChange}
             />
             <input
                 type="email"
                 placeholder="Email"
+                value={email}
+                onChange={onChange}
             />
             <input
                 type="password"
                 placeholder="Password"
+                value={password1}
+                onChange={onChange}
             />
             <input
                 type="password"
                 placeholder="Repeat Password"
+                value={password2}
+                onChange={onChange}
             />
             <button type="submit">Create</button>
             </form>
