d9acbe71e8d12764d65406c01948a59b28abf2be
jonathan <yoni_12rrs@hotmail.com>
2022-01-07 16:37:46 -0600

    Añadir props y controles a nuestra historia y componente

--- 11-my-storybook/src/stories/components/MyLabel.stories.tsx
+++ 11-my-storybook/src/stories/components/MyLabel.stories.tsx
@@ -1,14 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { MyLabel } from "../../components/MyLabel";
+
 
 export default {
     title: 'UI/MyLabel',
     component: MyLabel
-}
+} as ComponentMeta<typeof MyLabel>
 
-const Template = () => <MyLabel />
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />
 
 /* se crea una complia del template, para tener diferentes  historias */
 export const Basic = Template.bind({});
+Basic.args = {
+    size: 'normal'
+}
 
 export const AllCaps = Template.bind({});
 
