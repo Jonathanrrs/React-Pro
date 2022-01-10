330bdf500975185edd7f3c9b8c5da0b6e5d43a79
jonathan <yoni_12rrs@hotmail.com>
2022-01-10 17:43:07 -0600

    Manual - Network first with cache fallback - Parte 2

--- 12-react-calendar-frontend-pwa/src/service-worker.js
+++ 12-react-calendar-frontend-pwa/src/service-worker.js
@@ -88,23 +88,28 @@ self.addEventListener('fetch', (event) => {
 
   // console.log(event.request.url);
   
-  /* esto es network only */
+
   if(event.request.url !== 'http://localhost:4000/api/auth/renew') return;
   
   /* se recomienda trabajar con la manera mas pura(fetch) */
   const resp = fetch(event.request)
     .then(response => {
 
+      /* guardar en cache la respuesta */
+      caches.open('cache-dynamic')
+        .then(cache => {
+          cache.put(event.request, response)
+        })
 
       /* clonarla para usarla mas de una vez */
       return response.clone();
     })
     .catch(err => {
       console.log('offline response');
-      // return caches.match
+      return caches.match(event.request)
     })
 
-    event.responseWith(resp);
+    event.respondWith(resp);
 
   
 
