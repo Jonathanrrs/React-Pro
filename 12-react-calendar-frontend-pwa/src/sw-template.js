3c0fad8f8c085c835e3c3432f269db83f0d70792
jonathan <yoni_12rrs@hotmail.com>
2022-01-10 22:04:43 -0600

    Verificación de Token offline

--- 12-react-calendar-frontend-pwa/src/sw-template.js
+++ 12-react-calendar-frontend-pwa/src/sw-template.js
@@ -3,7 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
+const {CacheFirst, NetworkFirst} = workbox.strategies;
 
 registerRoute(
     new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
@@ -13,4 +13,14 @@ registerRoute(
 registerRoute(
     new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"'),
     new CacheFirst()
+)
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/auth/renew'),
+    new NetworkFirst()
+)
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/events'),
+    new NetworkFirst()
 )
