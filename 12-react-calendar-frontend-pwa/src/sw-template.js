fbb0d58d22551eebe8de00a69c4f8d5f872f63ba
jonathan <yoni_12rrs@hotmail.com>
2022-01-10 22:26:18 -0600

    Background Sync - Posteos sin conexión

--- 12-react-calendar-frontend-pwa/src/sw-template.js
+++ 12-react-calendar-frontend-pwa/src/sw-template.js
@@ -1,9 +1,14 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
+workbox.loadModule('workbox-background-sync');
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const {registerRoute} = workbox.routing;
-const {CacheFirst, NetworkFirst} = workbox.strategies;
+const {CacheFirst, NetworkFirst, NetworkOnly} = workbox.strategies;
+
+
+const {BackgroundSyncPlugin} = workbox.backgroundSync;
 
 registerRoute(
     new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
@@ -23,4 +28,17 @@ registerRoute(
 registerRoute(
     new RegExp('http://localhost:4000/api/events'),
     new NetworkFirst()
+)
+
+/* Posteos offline */
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+    maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+  });
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/events'),
+    new NetworkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'POST'
 )
