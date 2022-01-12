c2ebfdd818b3e87a47780d393775890b00d78d41
jonathan <yoni_12rrs@hotmail.com>
2022-01-11 20:16:38 -0600

    Crear el contexto de lugares

---
+++ 13-maps-app/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MapsApp } from './MapsApp';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MapsApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
