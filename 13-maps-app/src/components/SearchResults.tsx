b9a4a4fba44130e5449709e19b97a2bb849077a6
jonathan <yoni_12rrs@hotmail.com>
2022-01-13 17:19:24 -0600

    Probar el API desde React

--- 13-maps-app/src/components/SearchResults.tsx
+++ 13-maps-app/src/components/SearchResults.tsx
@@ -5,8 +5,8 @@ import { Feature } from '../interfaces/places';
 
 export const SearchResults = () => {
 
-    const { places, isLoadingPlaces } = useContext(PlacesContext);
-    const {map} = useContext(MapContext);
+    const { places, isLoadingPlaces, userLocation } = useContext(PlacesContext);
+    const {map, getRouteBetweenPoints} = useContext(MapContext);
 
     const [activeId, setActiveId] = useState('');
 
@@ -17,6 +17,12 @@ export const SearchResults = () => {
             zoom: 14,
             center: [lng, lat]
         })
+    }
+
+    const getRoute = (place: Feature) => {
+        if(!userLocation) return;
+        const [lng, lat] = place.center;
+        getRouteBetweenPoints(userLocation, [lng, lat])
     }
 
     if(isLoadingPlaces) {
@@ -44,7 +50,9 @@ export const SearchResults = () => {
                         >
                             {place.place_name}
                         </p>
-                        <button className={`btn btn-outline-primary btn-sm ${activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'}`}>
+                        <button 
+                        onClick={() => getRoute(place)}
+                        className={`btn btn-outline-primary btn-sm ${activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'}`}>
                             Direcciones
                         </button>
                     </li>
--- 13-maps-app/src/context/map/MapProvider.tsx
+++ 13-maps-app/src/context/map/MapProvider.tsx
@@ -4,6 +4,8 @@ import { Map, Marker, Popup } from 'mapbox-gl';
 import { MapContext } from './MapContext';
 import { mapReducer } from './mapReducer';
 import { PlacesContext } from '../';
+import {DirectionsApi} from '../../apis';
+import { DirectionsResponse } from '../../interfaces/directions';
 
 export interface MapState {
     isMapReady: boolean;
@@ -73,6 +75,16 @@ export const MapProvider = ({ children }: Props) => {
     }
 
     const getRouteBetweenPoints = async(start: [number, number], end: [number, number]) => {
+        const resp = await DirectionsApi.get<DirectionsResponse>(`/${start.join(',')};${end.join(',')}`);
+        const {distance, duration, geometry} = resp.data.routes[0];
+        
+        let kms = distance/1000;
+            kms = Math.round(kms * 100);
+            kms /= 100;
+
+        const minutes = Math.floor(duration/60);
+        console.log({distance, minutes});
+        
 
     }
 
