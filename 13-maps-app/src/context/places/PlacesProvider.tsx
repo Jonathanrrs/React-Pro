a469a24bd0c38f6231fda49be3ffa221faad4b06
jonathan <yoni_12rrs@hotmail.com>
2022-01-13 15:15:44 -0600

    Colocar el tipo de dato y almacenarlo en en el state

--- 13-maps-app/src/context/places/PlacesProvider.tsx
+++ 13-maps-app/src/context/places/PlacesProvider.tsx
@@ -3,15 +3,20 @@ import { PlacesContext } from './PlacesContext';
 import { placesReducer } from './placesReducer';
 import { getUserLocation } from '../../helpers/getUserLocation';
 import { SearchApi } from '../../apis';
+import { PlacesResponse, Feature } from '../../interfaces/places';
 
 export interface PlacesState {
     isLoading: boolean;
     userLocation?: [number, number];
+    isLoadingPlaces: boolean;
+    places:Feature[];
 }
 
 const INITIAL_STATE: PlacesState = {
     isLoading: true,
-    userLocation: undefined
+    userLocation: undefined,
+    isLoadingPlaces: false,
+    places:[]
 }
 
 interface Props {
@@ -27,18 +32,20 @@ export const PlacesProvider = ({children}: Props) => {
         .then(lngLat => dispatch({type: 'setUserLocation', payload: lngLat}))
     }, []);
 
-    const searchPlacesByTerm = async(query: string) => {
+    const searchPlacesByTerm = async(query: string): Promise<Feature[]> => {
         if(query.length === 0) return []; /* limpiar state */
         if(!state.userLocation)  throw new Error('No hay ubicación del usuario');
 
-        const resp = await SearchApi.get(`${query}.json`, {
+        dispatch({type: 'setLoadingPlaces'});
+
+        const resp = await SearchApi.get<PlacesResponse>(`${query}.json`, {
             params: {
                 proximity: state.userLocation?.join(',')
             }
         });
 
-        console.log(resp.data);
-        return resp.data
+        dispatch({type: 'setPlaces', payload: resp.data.features})
+        return resp.data.features;
         
     }
 
